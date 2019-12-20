d0b8830a2a139afcf403acf9243b3cd091e69f2c
Chisho Takemura <cisho0624donald@icloud.com>
2019-12-20 14:19:36 -0800

    Updates

---
+++ precache-manifest.118cb87760863b89be6f44fd02c8de74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7e9e6c0a5489f547e3a602cf4ea27b",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "6ec91965083d586874e3",
+    "url": "/contactmanager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6ec91965083d586874e3",
+    "url": "/contactmanager/static/js/2.96d73daf.chunk.js"
+  },
+  {
+    "revision": "c8336e0bcbf8ea1670aafb15aa044bf7",
+    "url": "/contactmanager/static/js/2.96d73daf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b79210159655c106f643",
+    "url": "/contactmanager/static/js/main.9f836595.chunk.js"
+  },
+  {
+    "revision": "6d5470f28a9dc2aa0ea3",
+    "url": "/contactmanager/static/js/runtime-main.fef8aa86.js"
+  }
+]);
